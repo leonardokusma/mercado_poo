37803b9772b6564d542c3206b2b1b46a1b680e5d
leonardo kusma <leonardokusma48@gmail.com>
2024-06-19 18:48:02 -0300

    m

--- src/main/java/crud/MFuncionario.java
+++ src/main/java/crud/MFuncionario.java
@@ -4,10 +4,67 @@
  */
 package crud;
 
+import java.util.Date;
+
 /**
  *
  * @author leona
  */
-public class MFuncionario {
-    
+public class MFuncionario extends MPessoa {
+
+    private int idFuncionario;
+    private int idCargo;
+    private int idEndereco;
+    private int idContato;
+    private Date admissao;
+
+    public MFuncionario(String nome, String cpf, Date nascimento, int idFuncionario, int idCargo, int idEndereco, int idContato, Date admissao) {
+        super(nome, cpf, nascimento);
+        this.idFuncionario = idFuncionario;
+        this.idCargo = idCargo;
+        this.idEndereco = idEndereco;
+        this.idContato = idContato;
+        this.admissao = admissao;
+    }
+
+    public int getIdFuncionario() {
+        return idFuncionario;
+    }
+
+    public void setIdFuncionario(int idFuncionario) {
+        this.idFuncionario = idFuncionario;
+    }
+
+    public int getIdCargo() {
+        return idCargo;
+    }
+
+    public void setIdCargo(int idCargo) {
+        this.idCargo = idCargo;
+    }
+
+    public int getIdEndereco() {
+        return idEndereco;
+    }
+
+    public void setIdEndereco(int idEndereco) {
+        this.idEndereco = idEndereco;
+    }
+
+    public int getIdContato() {
+        return idContato;
+    }
+
+    public void setIdContato(int idContato) {
+        this.idContato = idContato;
+    }
+
+    public Date getAdmissao() {
+        return admissao;
+    }
+
+    public void setAdmissao(Date admissao) {
+        this.admissao = admissao;
+    }
+
 }
