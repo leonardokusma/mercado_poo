a297bb3fbbabe93ded26899e3b4baf97104fd792
leonardo kusma <leonardokusma48@gmail.com>
2024-06-22 00:49:52 -0300

    [ADD] classes estoqueProduto HistoricoVenda

--- src/main/java/dominio/MerEstoqueProduto.java
+++ src/main/java/dominio/MerEstoqueProduto.java
@@ -1,13 +1,85 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dominio;
 
-/**
- *
- * @author leona
- */
 public class MerEstoqueProduto {
-    
+
+    private static long id = 1;
+    private long idMerEstoqueProduto;
+    private double precoUnitario;
+    private int qtd;
+    private MerProduto produto;
+    private MerEstoque estoque;
+    private MerFornePrincipal forne;
+    private MerForneAlternativo forneAlternativo;
+
+    public MerEstoqueProduto(double preco, int qtd, MerProduto produto, MerEstroque estoque, MerFornePrincipal forne, MerForneAlternativo forneAlternativo) {
+        this.idMerEstoqueProduto = MerEstoqueProduto.id;
+        MerEstoqueProduto.setId(id++);
+        this.precoUnitario = preco;
+        this.qtd = qtd;
+        this.produto = produto;
+        this.estoque = estoque;
+        this.forne = forne;
+        this.forneAlternativo = forneAlternativo;
+    }
+
+    public static void setId(long id) {
+        MerEstoqueProduto.id = id;
+    }
+
+    public long getIdMerEstoqueProduto() {
+        return idMerEstoqueProduto;
+    }
+
+    public void setIdMerEstoqueProduto(long idMerEstoqueProduto) {
+        this.idMerEstoqueProduto = idMerEstoqueProduto;
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public int getQtd() {
+        return qtd;
+    }
+
+    public void setQtd(int qtd) {
+        this.qtd = qtd;
+    }
+
+    public MerProduto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(MerProduto produto) {
+        this.produto = produto;
+    }
+
+    public MerEstoque getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(MerEstoque estoque) {
+        this.estoque = estoque;
+    }
+
+    public MerFornePrincipal getForne() {
+        return forne;
+    }
+
+    public void setForne(MerFornePrincipal forne) {
+        this.forne = forne;
+    }
+
+    public MerForneAlternativo getForneAlternativo() {
+        return forneAlternativo;
+    }
+
+    public void setForneAlternativo(MerForneAlternativo forneAlternativo) {
+        this.forneAlternativo = forneAlternativo;
+    }
+
 }
--- src/main/java/dominio/MerHistoricoSaida.java
+++ src/main/java/dominio/MerHistoricoSaida.java
@@ -1,14 +1,13 @@
-
 package dominio;
 
-
 public class MerHistoricoSaida {
+
     private static long id = 1;
     private long idMerHistoricoSaida;
     private MerEstoqueProduto produto;
     private MerVenda venda;
-    
-    public MerHistoricoSaida(MerEStoqueProduto produto, MerVenda venda){
+
+    public MerHistoricoSaida(MerEStoqueProduto produto, MerVenda venda) {
         this.idMerHistoricoSaida = MerHistoricoSaida.id;
         MerHistoricoSaida.setId(id++);
         this.produto = produto;
@@ -18,6 +17,29 @@ public class MerHistoricoSaida {
     public static void setId(long id) {
         MerHistoricoSaida.id = id;
     }
-    
-    
+
+    public long getIdMerHistoricoSaida() {
+        return idMerHistoricoSaida;
+    }
+
+    public void setIdMerHistoricoSaida(long idMerHistoricoSaida) {
+        this.idMerHistoricoSaida = idMerHistoricoSaida;
+    }
+
+    public MerEstoqueProduto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(MerEstoqueProduto produto) {
+        this.produto = produto;
+    }
+
+    public MerVenda getVenda() {
+        return venda;
+    }
+
+    public void setVenda(MerVenda venda) {
+        this.venda = venda;
+    }
+
 }
